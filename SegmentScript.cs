a811690715624fccb34fa701e161644449762af3
matissolus <matiss2000@inbox.lv>
2023-05-31 18:46:34 +0100

    Began adding obstacles

--- SegmentScript.cs
+++ SegmentScript.cs
@@ -3,26 +3,26 @@ using System;
 
 public partial class SegmentScript : Node3D
 {
-    public async void _on_Area_body_exited(CharacterBody3D body)
-    {
-        if (body is Player pBody)
-        {
-            if (pBody.Speed > 100)
-            {
-                SegmentSpawner parent = (SegmentSpawner)GetParent();
-                parent.InstanceList.Remove((Node3D)parent.GetChild(0));
-                QueueFree();
-                GD.Print("killed");
-            }
-            else
-            {
-                GD.Print("start");
-                await ToSignal(GetTree().CreateTimer(pBody.Speed / (1.5f * pBody.Speed)), SceneTreeTimer.SignalName.Timeout);
-                SegmentSpawner parent = (SegmentSpawner)GetParent();
-                parent.InstanceList.Remove((Node3D)parent.GetChild(0));
-                QueueFree();
-                GD.Print("end");
-            }
-        }
-    }
+	public async void _on_Area_body_exited(CharacterBody3D body)
+	{
+		if (body is Player pBody)
+		{
+			if (pBody.Speed > 100)
+			{
+				SegmentSpawner parent = (SegmentSpawner)GetParent();
+				parent.InstanceList.Remove((Node3D)parent.GetChild(0));
+				QueueFree();
+				GD.Print("killed");
+			}
+			else
+			{
+				GD.Print("start");
+				await ToSignal(GetTree().CreateTimer(pBody.Speed / (1.5f * pBody.Speed)), SceneTreeTimer.SignalName.Timeout);
+				SegmentSpawner parent = (SegmentSpawner)GetParent();
+				parent.InstanceList.Remove((Node3D)parent.GetChild(0));
+				QueueFree();
+				GD.Print("end");
+			}
+		}
+	}
 }
--- SegmentSpawner.cs
+++ SegmentSpawner.cs
@@ -9,99 +9,99 @@ using System.Threading;
 
 public partial class SegmentSpawner : Node3D
 {
-    PackedScene CircleSegment;
-    PackedScene LineSegment;
-    PackedScene ExpandedSegment;
-    RandomNumberGenerator rng;
+	PackedScene CircleSegment;
+	PackedScene LineSegment;
+	PackedScene ExpandedSegment;
+	RandomNumberGenerator rng;
 
-    Dictionary SegmentDict;
-    Godot.Collections.Array keyArray;
-    Vector3 addedDistance = new Vector3(0, 0, 100);
-    int lastKey = 0;
-    int randomKey = 0;
-    Player Player;
-    float SpawnTime = 2f;
-    public List<Node3D> InstanceList;
+	Dictionary SegmentDict;
+	Godot.Collections.Array keyArray;
+	Vector3 addedDistance = new Vector3(0, 0, 100);
+	int lastKey = 0;
+	int randomKey = 0;
+	Player Player;
+	float SpawnTime = 2f;
+	public List<Node3D> InstanceList;
 
-    public override void _Ready()
-    {
-        InstanceList = new List<Node3D>();
-        GD.Randomize();
-        rng = new RandomNumberGenerator();
-        rng.Randomize();
-        CircleSegment = ResourceLoader.Load<PackedScene>("res://CirclesSegmentWithAudio.tscn");
-        LineSegment = ResourceLoader.Load<PackedScene>("res://LinesSegment.tscn");
-        ExpandedSegment = ResourceLoader.Load<PackedScene>("res://ExpandedSegmentWithAudio.tscn");
-        SegmentDict = new Dictionary();
+	public override void _Ready()
+	{
+		InstanceList = new List<Node3D>();
+		GD.Randomize();
+		rng = new RandomNumberGenerator();
+		rng.Randomize();
+		CircleSegment = ResourceLoader.Load<PackedScene>("res://CirclesSegmentWithAudio.tscn");
+		LineSegment = ResourceLoader.Load<PackedScene>("res://LinesSegment.tscn");
+		ExpandedSegment = ResourceLoader.Load<PackedScene>("res://ExpandedSegmentWithAudio.tscn");
+		SegmentDict = new Dictionary();
 
-        SegmentDict.Add(1, (PackedScene)CircleSegment);
-        SegmentDict.Add(2, (PackedScene)LineSegment);
-        SegmentDict.Add(3, (PackedScene)ExpandedSegment);
-        keyArray = new Godot.Collections.Array(SegmentDict.Keys);
-        CallDeferred("SpawnSegment");
-        Player = GetNode<Player>("%Player");
+		SegmentDict.Add(1, (PackedScene)CircleSegment);
+		SegmentDict.Add(2, (PackedScene)LineSegment);
+		SegmentDict.Add(3, (PackedScene)ExpandedSegment);
+		keyArray = new Godot.Collections.Array(SegmentDict.Keys);
+		CallDeferred("SpawnSegment");
+		Player = GetNode<Player>("%Player");
 
-    }
-    public override void _Process(double delta)
-    {
-        if (InstanceList.Count < 4)
-        {
-            GD.Print(InstanceList.Count);
-            SpawnSegment();
-        }
-        if (Player.Position.Z < -10000)
-        {
-            foreach (Node3D child in InstanceList)
-            {
-                child.Position += new Vector3(0, 0, 10000);
-            }
-            Player.Position += new Vector3(0, 0, 10000);
-            addedDistance = lastKey == 2 ? new Vector3(0, 0, -160) : new Vector3(0, 0, -100);
-        }
-    }
+	}
+	public override void _Process(double delta)
+	{
+		if (InstanceList.Count < 4)
+		{
+			GD.Print(InstanceList.Count);
+			SpawnSegment();
+		}
+		if (Player.Position.Z < -10000)
+		{
+			foreach (Node3D child in InstanceList)
+			{
+				child.Position += new Vector3(0, 0, 10000);
+			}
+			Player.Position += new Vector3(0, 0, 10000);
+			addedDistance = lastKey == 2 ? new Vector3(0, 0, -160) : new Vector3(0, 0, -100);
+		}
+	}
 
-    public PackedScene ChooseSegment()
-    {
-        GD.Randomize();
-        randomKey = rng.RandiRange(0, 2);
-        if (lastKey != randomKey)
-        {
-            var nextSeg = SegmentDict[keyArray[randomKey]];
-            return (PackedScene)nextSeg;
-        }
-        else
-        {
-            do
-            {
-                randomKey = rng.RandiRange(0, 2);
-            } while (lastKey == randomKey);
-            var nextSeg = SegmentDict[keyArray[randomKey]];
-            return (PackedScene)nextSeg;
-        }
-    }
+	public PackedScene ChooseSegment()
+	{
+		GD.Randomize();
+		randomKey = rng.RandiRange(0, 2);
+		if (lastKey != randomKey)
+		{
+			var nextSeg = SegmentDict[keyArray[randomKey]];
+			return (PackedScene)nextSeg;
+		}
+		else
+		{
+			do
+			{
+				randomKey = rng.RandiRange(0, 2);
+			} while (lastKey == randomKey);
+			var nextSeg = SegmentDict[keyArray[randomKey]];
+			return (PackedScene)nextSeg;
+		}
+	}
 
-    public void SpawnSegment()
-    {
-        var SegmentInstance = ChooseSegment().Instantiate<Node3D>();
-        if (lastKey == 2)
-        {
-            addedDistance.Z += -160;
-        }
-        else
-        {
-            addedDistance.Z += -100;
-        }
-        SegmentInstance.Position += addedDistance;
-        GD.Print(addedDistance);
+	public void SpawnSegment()
+	{
+		var SegmentInstance = ChooseSegment().Instantiate<Node3D>();
+		if (lastKey == 2)
+		{
+			addedDistance.Z += -160;
+		}
+		else
+		{
+			addedDistance.Z += -100;
+		}
+		SegmentInstance.Position += addedDistance;
+		GD.Print(addedDistance);
 
-        if (SegmentInstance.Position.DistanceTo(Player.Position) < 500)
-        {
-            InstanceList.Add(SegmentInstance);
-            AddChild(SegmentInstance);
-            lastKey = randomKey;
-        }
+		if (SegmentInstance.Position.DistanceTo(Player.Position) < 500)
+		{
+			InstanceList.Add(SegmentInstance);
+			AddChild(SegmentInstance);
+			lastKey = randomKey;
+		}
 
-    }
+	}
 
 
 }
